4e29e7c58fcc10f47d24a177a2247574092fbff0
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-10-05 18:25:06 +0300

    Add project files.

---
+++ BroadcastMessengerConsole/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using ClientServer;
+using Microsoft.Win32;
+
+namespace BroadcastMessengerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("127.0.0.1",8000);
+            server.Start();
+            Task task = new Task(()=>server.ConnectionUpdate());
+            task.Start();
+            while (true)
+            {
+                //   Mouse.SetCursorPos(0,0);
+                if (server.handler.Count != 0)
+                {
+
+                    Console.WriteLine("1.Start App\n2.Remove User\n3.Change Client Window Size\n4.Change Client IP");
+                    Console.Write("MSG: ");
+                    string str = Console.ReadLine();
+                    switch (str) {
+                        case "1":
+                            Console.Write("Enter path:");
+                            str = Console.ReadLine();
+                            break;
+                        case "2":
+                            for (int i = 0; i < server.handler.Count; i++) {
+                                Console.WriteLine(i + ". " + server.handler[i].socket.RemoteEndPoint.ToString());
+                            }
+                            Console.WriteLine("Enter index:");
+                            str = Console.ReadLine();
+                            if (str.Length >= 1 && str.All(c => char.IsDigit(c))) {
+                                if (int.Parse(str) >= 0 && int.Parse(str) < server.handler.Count)
+                                {
+                                    server.RemoveClient(int.Parse(str));
+                                }
+                            }
+                            str = "--end--";
+                            break;
+
+                        case "3":
+                            Console.WriteLine("Write Width:");
+                            var w = Console.ReadLine();
+                            Console.WriteLine("Write Height:");
+                            var h = Console.ReadLine();
+
+                            RegistryKey key = Registry.CurrentUser;
+                            if (w.Length != 0 && h.Length != 0)
+                            {
+                                if (w.All(c => char.IsDigit(c)) && h.All(c => char.IsDigit(c)))
+                                {
+
+                                    if (key.GetSubKeyNames().Contains("ClientData"))
+                                    {
+                                        RegistryKey KeyData = key.OpenSubKey("ClientData", true);
+                                        KeyData.SetValue("Height", w);
+                                        KeyData.SetValue("Width", h);
+
+                                        str = "--size " + w + " " + h;
+                                        KeyData.Close();
+                                    }
+                                }
+                            }
+                            break;
+                        case "4":
+                            Console.WriteLine("Write new IP:");
+                            var ipclient = Console.ReadLine();
+
+                            RegistryKey keyclient = Registry.CurrentUser;
+
+                            if (IPAddress.Parse(ipclient) != null)
+                            {
+
+                                if (keyclient.GetSubKeyNames().Contains("ClientData"))
+                                {
+                                    RegistryKey KeyData = keyclient.OpenSubKey("ClientData", true);
+                                    KeyData.SetValue("serverip", ipclient);
+
+                                    str = "--end--";
+                                    KeyData.Close();
+                                }
+                            }
+                    
+
+
+
+                            break;
+                        default:
+                            str = "--end--";
+                            break;
+                    }
+                    if (str == "--end--") {
+                        continue;
+                    }
+                    for (int i = 0; i < server.handler.Count; i++)
+                    {
+                        server.Send(Server.FromStringToBytes(str), i);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+            server.Close();
+        }
+
+
+
+
+    }
+
+
+
+
+}
+
+public class Mouse {
+    [DllImport("user32.dll")]
+    public static extern long SetCursorPos(int x, int y);
+    [DllImport("user32.dll")]
+    public static extern long ClientToScreen(IntPtr hWnd, ref POINT point);
+    [DllImport("user32.dll", SetLastError = false)]
+    public static extern IntPtr GetDesktopWindow();
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT {
+        public int x;
+        public int y;
+        public POINT(int X,int Y)
+        {
+            x = X;
+            y = Y;
+
+        }
+    }
+
+
+}
---
+++ Client/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using ClientServer;
+using Microsoft.Win32;
+
+namespace Clients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = string.Empty;
+            string pass = string.Empty;
+            string ip = "127.0.0.1";
+
+
+            RegistryKey key = Registry.CurrentUser;
+            //key.DeleteSubKey("ClientData");
+
+            if (key.GetSubKeyNames().Contains("ClientData")) {
+                RegistryKey KeyData=key.OpenSubKey("ClientData");
+                var h = int.Parse(KeyData.GetValue("Height").ToString());
+                var w = int.Parse(KeyData.GetValue("Width").ToString());
+                ip = KeyData.GetValue("serverip").ToString();
+                Console.SetWindowSize(w , h);
+                KeyData.Close();
+            }
+            else{
+                RegistryKey newKeyData = key.CreateSubKey("ClientData", true);
+
+                newKeyData.SetValue("Height",Console.WindowHeight);
+                newKeyData.SetValue("Width", Console.WindowWidth);
+                newKeyData.SetValue("serverip", "127.0.0.1");
+                newKeyData.Close();
+            }
+
+
+            Client client = new Client(ip,8000);
+            client.Connect();
+            client.Send(Client.FromStringToBytes(client.ToString()));
+            while (true)
+            {
+                var data = Client.FromBytesToString(client.Get());
+                if (data.Split(" ").Length >= 2) {
+                    var components = data.Split(" ");
+                    if (components[0] == "--start") {
+                        var reference = data.Remove(0,components[0].Length);
+                        Process.Start(reference.ToString());
+                        continue;
+                    }
+                   
+                    if (components[0] == "--size")
+                    {
+
+                        Console.SetWindowSize(int.Parse(components[1].ToString()), int.Parse(components[2].ToString()));
+                        continue;
+                    }
+                }
+                Console.WriteLine(data);
+            }
+            Console.ReadLine();
+
+            client.Close();
+        }
+    }
+    public class Mouse
+    {
+        [DllImport("user32.dll")]
+        public static extern long SetCursorPos(int x, int y);
+        [DllImport("user32.dll")]
+        public static extern long ClientToScreen(IntPtr hWnd, ref POINT point);
+        [DllImport("user32.dll", SetLastError = false)]
+        public static extern IntPtr GetDesktopWindow();
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+            public POINT(int X, int Y)
+            {
+                x = X;
+                y = Y;
+
+            }
+        }
+
+
+    }
+}
